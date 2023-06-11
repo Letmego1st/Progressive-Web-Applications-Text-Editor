602ac68bd59fa6aadfe049d4e0a7730864157866
letmego1st <imcoding4real@gmail.com>
2023-06-11 12:02:32 -0700

    added code to install.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
+// Create a connection to the database
+const db = await initdb();
+
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,19 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the mini-infobar from appearing on mobile
+  // Stash the event so it can be triggered later.
+  event.preventDefault();
+  deferredPrompt = event;
+  installButton.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {});
+event.preventDefault();
+// Show the install prompt
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {});
