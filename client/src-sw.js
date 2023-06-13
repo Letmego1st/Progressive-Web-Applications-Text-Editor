514a634e3b7c450a722032f1afd2c00ed4fe947a
letmego1st <imcoding4real@gmail.com>
2023-06-12 22:03:23 -0700

    corrected package.json

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,11 +1,12 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
+//const precacheManifest = self.__WB_MANIFEST || []
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -35,6 +36,10 @@ registerRoute(
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        maxEntries: 60,
       }),
     ],
   })
