baa47bcd1384ba8e59aeeec802e2ea13ce186538
letmego1st <imcoding4real@gmail.com>
2023-06-12 18:44:20 -0700

    added putDb and getDb code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,13 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// Create a connection to the database
-const db = await initdb();
-
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ content });
+  await tx.done;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.get(1);
+  await tx.done;
+  return content;
+};
 
 initdb();
