26a80e6466f5dd4a043474b00c904a29537f548c
letmego1st <imcoding4real@gmail.com>
2023-06-12 18:19:29 -0700

    added cssLoader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,7 +47,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
